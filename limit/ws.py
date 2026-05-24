dcd347acf7369e8b0c78d4d4531a0fa93a4f1525
WAHYUSTORE17 <142437082+WAHYUSTORE17@users.noreply.github.com>
2026-05-25 00:29:32 +0700

    Update ws.py

--- limit/ws.py
+++ limit/ws.py
@@ -13,7 +13,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:143'
-RESPONSE = 'HTTP/1.1 101 <b><i><font color="blue">Rerechan02</font></b> Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: foo\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <b><i><font color="green">Wahyu Store</font></b> Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: foo\r\n\r\n'
 
 SOCKS_VERSION = 5
 
